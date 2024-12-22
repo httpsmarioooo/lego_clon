44945bd7e836a5c126b212dc01512188a397d327
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-12-22 16:43:21 -0500

    Navbar Terminado

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  let inputSearch = false;
+  function openSearch(){
+    if(!inputSearch){
+      document.getElementById('btnSearch').classList.add('d-none');
+      document.getElementById('inputSearch').classList.remove('d-none')
+      document.getElementById('box-shadow').innerHTML = '<div style="z-index:0" onclick="openSearch()" class = "modal-backdrop fade show"></div>';
+      inputSearch = true;
+    }else{
+      document.getElementById('btnSearch').classList.remove('d-none');
+      document.getElementById('inputSearch').classList.add('d-none')
+      document.getElementById('box-shadow').innerHTML = '';
+      inputSearch = false;
+    }
+  }
