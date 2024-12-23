667c19e039ec42a6bd188329945a1f77eddb5d6b
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-12-23 02:36:49 -0500

    update arreglado

--- app.js
+++ app.js
@@ -11,12 +11,31 @@ var swiper = new Swiper(".mySwiper", {
     if(!inputSearch){
       document.getElementById('btnSearch').classList.add('d-none');
       document.getElementById('inputSearch').classList.remove('d-none')
-      document.getElementById('box-shadow').innerHTML = '<div style="z-index:0" onclick="openSearch()" class = "modal-backdrop fade show"></div>';
+      document.getElementById('boxx-shadow').innerHTML = '<div style="z-index:0" onclick="openSearch()" class = "modal-backdrop fade show"></div>';
       inputSearch = true;
     }else{
       document.getElementById('btnSearch').classList.remove('d-none');
       document.getElementById('inputSearch').classList.add('d-none')
-      document.getElementById('box-shadow').innerHTML = '';
+      document.getElementById('boxx-shadow').innerHTML = '';
       inputSearch = false;
     }
   }
+
+  function closeTopbar(){
+    let element = document.getElementById('topbar');
+  
+    if(!element.classList.contains('d-none')){
+      element.classList.add('d-none');
+    }
+  }
+  
+  window.addEventListener('resize',()=>{
+    console.log(window.innerWidth);
+    let element = document.getElementById('topbar');
+  
+    if(element.classList.contains('d-none')){
+      if(window.innerWidth >= 992){
+        element.classList.remove('d-none');
+      }
+    }
+  });
